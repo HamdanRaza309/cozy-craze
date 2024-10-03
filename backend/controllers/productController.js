80daaa47a09e2a5e25c93dd3f6341b9081116a5e
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-10-03 10:06:15 +0500

    Implement product addition functionality with multiple image uploads using Cloudinary and multer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,47 @@
-
-
-
+import { v2 as cloudinary } from 'cloudinary';
+import productModel from "../models/productModel.js";
 // add product
-const addProduct = async (req, res) => { }
+const addProduct = async (req, res) => {
+    try {
+        const { name, description, price, category, subCategory, sizes, bestseller } = req.body;
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
+
+        const images = [image1, image2, image3, image4].filter((image) => (
+            image !== undefined
+        ))
+
+        let imagesUrl = await Promise.all(
+            images.map(async (item) => {
+                let result = await cloudinary.uploader.upload(item.path, { resource_type: 'image' });
+                return result.secure_url;
+            })
+        )
+
+        const productData = {
+            name,
+            description,
+            price: Number(price),
+            category,
+            subCategory,
+            sizes: JSON.parse(sizes),
+            bestseller: bestseller === "true" ? true : false,
+            images: imagesUrl,
+            date: Date.now()
+        }
+        console.log(productData);
+
+        const product = new productModel(productData)
+        await product.save();
+
+        res.json({ success: true, message: 'Product Added' })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
 
 // list products
 const listProducts = async (req, res) => { }

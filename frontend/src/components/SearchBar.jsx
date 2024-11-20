2c25d0181614d2f849d3fc6b4bc2e011d6d3cd10
Hamdan Raza <142483895+HamdanRaza309@users.noreply.github.com>
2024-11-21 00:34:55 +0500

    Make SearchBar component responsive for smaller screens.

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,11 +1,10 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from '../context/ShopContext'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 import { useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCross, faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function SearchBar() {
-
     const { search, setSearch, showSearch, setShowSearch } = useContext(ShopContext);
     const location = useLocation();
     const [visible, setVisible] = useState(false);
@@ -13,22 +12,29 @@ function SearchBar() {
     useEffect(() => {
         if (location.pathname.includes('collection') && showSearch) {
             setVisible(true);
-        }
-        else {
+        } else {
             setVisible(false);
         }
-    }, [location])
-
+    }, [location, showSearch]);
 
     return showSearch && visible ? (
-        <div className="border-t border-b bg-gray-50 text-center">
-            <div className="inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-2/3 sm:w-1/2">
-                <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 outline-none bg-inherit text-sm' type="text" placeholder='Search' />
-                <FontAwesomeIcon icon={faSearch} className='w-4' />
+        <div className="border-t border-b bg-gray-50 text-center p-3">
+            <div className="flex items-center justify-between border border-gray-400 px-3 py-2 mx-auto rounded-full w-full max-w-lg">
+                <input
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="flex-1 outline-none bg-transparent text-sm sm:text-base"
+                    type="text"
+                    placeholder="Search"
+                />
+                <FontAwesomeIcon
+                    icon={faXmark}
+                    className="ml-3 w-6 sm:w-8 text-gray-500 cursor-pointer"
+                    onClick={() => setShowSearch(false)}
+                />
             </div>
-            <FontAwesomeIcon onClick={() => setShowSearch(false)} icon={faXmark} className='inline w-3 cursor-pointer' />
         </div>
-    ) : null
+    ) : null;
 }
 
-export default SearchBar
+export default SearchBar;
